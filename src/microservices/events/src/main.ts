9f43589151df184e087848f64e12b3dba4ff50ae
iim <i.mandanova@bteleport.ru>
2025-12-09 09:46:12 +0800

    fix(events-service): connect Kafka

--- src/microservices/events/src/main.ts
+++ src/microservices/events/src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
     transport: Transport.KAFKA,
     options: {
       client: { brokers: ['kafka:9092'] },
+      consumer: {
+        groupId: 'events-service-group',
+        allowAutoTopicCreation: true,
+      },
+      producer: {
+        allowAutoTopicCreation: true,
+      },
     },
   });
 
