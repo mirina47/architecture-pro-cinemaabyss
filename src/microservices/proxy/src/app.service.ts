45cdb49d24f23dc421cc88c497d376f646668711
iim <i.mandanova@bteleport.ru>
2025-12-11 20:52:02 +0800

    fix(proxy-service): add migration with MOVIES_MIGRATION_PERCENT

--- src/microservices/proxy/src/app.service.ts
+++ src/microservices/proxy/src/app.service.ts
@@ -2,14 +2,33 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private requestCounter = 0;
+
+  migration(): boolean {
+    const percent = Number(process.env.MOVIES_MIGRATION_PERCENT || 0);
+
+    if (percent <= 0) return false;
+    if (percent >= 100) return true;
+
+    const g = this.gcd(percent, 100);
+    const bucketSize = 100 / g;
+    const moviesCount = percent / g;
+
+    const idx = this.requestCounter++ % bucketSize;
+    return idx < moviesCount;
+  }
+
+  private gcd(a: number, b: number): number {
+    return b === 0 ? a : this.gcd(b, a % b);
+  }
+
   async getMovies(): Promise<any> {
-    const requestOptions: RequestInit = {
-      method: 'GET',
-    };
-    const url = new URL(process.env.MONOLITH_URL + '/api/movies');
-    const response = await fetch(url.href, requestOptions);
+    const isMigration = this.migration();
+    const url = new URL((isMigration ? process.env.MOVIES_SERVICE_URL : process.env.MONOLITH_URL) + '/api/movies');
+    console.log(url.href);
+    const response = await fetch(url.href, { method: 'GET' });
     if (!response.ok) {
-      throw new Error(`Monolith error: ${response.status}`);
+      throw new Error(`${isMigration ? 'Movies' : 'Monolith'} error: ${response.status}`);
     }
     return response.json();
   }
